9c0d952bf03884d0d7c61f2a5a79353225d245db
Shree5vatsa <shreevatsa9124@gmail.com>
2025-06-24 16:24:46 +0545

    playBtn fix

--- src/components/PlayBtn.tsx
+++ src/components/PlayBtn.tsx
@@ -17,7 +17,7 @@ export function PlayBtn({
             onClick={handlerRunVisualizer}
             className="disabled:pointer-events-none disabled:opacity-50 transition ease-in rounded-full p-2.5 shadow-md bg-green-500 hover:bg_green-600 border-none active: ring-green-300 focus:outline-none focus:ring-green-300 focus-ring-opacity-30"
             >
-            {isPathNavigated? <GrPowerReset className="w-5 h-5"/>:<BsFillPlayFill className w-5 h-5/>}
+            {isPathNavigated? <GrPowerReset className="w-5 h-5"/>:<BsFillPlayFill className= "w-5 h-5"/>}
             </button>
     )
 }
